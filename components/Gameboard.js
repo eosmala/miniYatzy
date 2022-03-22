4ad6757c6b0dc4797c23fe5c58d0d58bcbe49106
Elina Osmala <n0osel00@students.oamk.fi>
2022-03-22 18:16:37 +0200

    Seems to work?

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { Text, View, Pressable } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Pressable, Alert } from 'react-native';
 import styles from '../style/style';
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { Col, Row, Grid } from "react-native-easy-grid";
 
 const board = [];
 const NBR_OF_DICES = 5;
 const NBR_OF_THROWS = 3;
 const NBR_OF_POINTS = 6;
+const POINTS_TO_BONUS = 63
 
 export default function Gameboard() {
     const [nrbOfThrowsLeft, setNrbOfThrowsLeft] = useState(NBR_OF_THROWS);
@@ -16,6 +16,8 @@ export default function Gameboard() {
     const [selectedPoints, setSelectedPoints] = useState(new Array(NBR_OF_POINTS).fill(false));
     const [sum, setSum] = useState(new Array(NBR_OF_POINTS+1).fill(0));
     const [pointIcon, setPointIcon] = useState([]);
+    const [pointTotal, setPointTotal] = useState(0);
+    const [bonus, setBonus] = useState("You are" + POINTS_TO_BONUS +"points away from bonus")
 
 
     // function to get all point icons
@@ -52,16 +54,39 @@ export default function Gameboard() {
 
     // A function to select points
     function selectPoints(i) {
-        if (nrbOfThrowsLeft === 0 && !selectedPoints[i]) {
+        if (selectedPoints[i]) {
+            setStatus('Point already chosen, choose different')
+        } else if (nrbOfThrowsLeft === 0) {
             let points = [...selectedPoints];
-            points[i] = selectedPoints[i] ? false : true;
+            points[i] = true;       
+            if (selectedPoints[0] === false) {
+                points[0] = true
+            }
             setSelectedPoints(points);
 
             let sums = [...sum];
             sums[i] = getSum(i);
             setSum(sums);
+            IsGameover();
             setNrbOfThrowsLeft(NBR_OF_THROWS);
             setSelectedDices(new Array(NBR_OF_DICES).fill(false));
+        } else {
+            setStatus('Throw 3 times before setting points')
+        }
+
+    }
+
+    // Calculate total points and points until bonus
+    function TotalPoints(){
+        let total = 0;
+        sum.forEach(n => total += n)
+        let toBonus = POINTS_TO_BONUS - total;
+        if (total >= POINTS_TO_BONUS) {
+            setPointTotal(total + 50)
+            setBonus("You got the bonus!")
+        } else {
+            setPointTotal(total);
+            setBonus("You are " + toBonus + " points away from bonus.")
         }
     }
 
@@ -76,7 +101,35 @@ export default function Gameboard() {
         }
             setNrbOfThrowsLeft(nrbOfThrowsLeft-1);
         } else {
-            setStatus("Choose points")
+            setStatus('Really, choose points')
+        }
+    }
+
+    function NewGame() {
+        setNrbOfThrowsLeft(NBR_OF_THROWS);
+        setSelectedDices(new Array(NBR_OF_DICES).fill(false));
+        setSelectedPoints(new Array(NBR_OF_POINTS).fill(false));
+        setSum(new Array(NBR_OF_POINTS+1).fill(0));
+        setPointTotal(0);
+        setBonus("You are " + POINTS_TO_BONUS +" points away from bonus.")
+    }
+
+    const showAlert = () => {
+        Alert.alert(
+            "Game over",
+            "You got " + pointTotal + " points, yay!  " + bonus,
+            [
+                {
+                    text: "New game",
+                    onPress: () => NewGame()
+                }
+            ]
+            );
+    }
+
+    function IsGameover(){
+        if (selectedPoints.every(val => val == true)) {
+            showAlert()
         }
     }
 
@@ -85,11 +138,13 @@ export default function Gameboard() {
             initializePointIcons();
             setStatus('Throw dices');
         } if (nrbOfThrowsLeft < NBR_OF_THROWS) {
-            setStatus("Keep throwing")
-        }if (nrbOfThrowsLeft < 0) {
-            setNrbOfThrowsLeft(NBR_OF_THROWS-1);
+            setStatus('Keep throwing');
+        } if (nrbOfThrowsLeft == 0) {
+            setStatus('Choose points');
         }
-    }, [nrbOfThrowsLeft]);
+        TotalPoints()
+        IsGameover()
+    }, [sum, nrbOfThrowsLeft]);
 
     // Row of dices
     const row = [];
@@ -117,7 +172,6 @@ export default function Gameboard() {
                 key={"numberRow" + i}
                 onPress={() => selectPoints(i)}
                 style={styles.points}
-                // disabled={pointIcon[i] === selectedPoints[i] ? "true" : "false"}
                 >
                 <Text>{sum[i]}</Text>
                 <MaterialCommunityIcons
@@ -139,6 +193,8 @@ export default function Gameboard() {
                 onPress={()=>throwDices()}>
                 <Text style={styles.buttonText}>Throw dices</Text>
             </Pressable>
+            <Text style={styles.total}>Total points: {pointTotal}</Text>
+            <Text style={styles.gameinfo}>{bonus}</Text>
             <View style={styles.flex}>{numberRow}</View>
         </View>
     )
